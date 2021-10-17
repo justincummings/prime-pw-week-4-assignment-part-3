5bfb7a15feaeeb22ca99e539cd14e6ccf320907d
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-10-17 14:49:26 -0500

    completed pt 3 problems 1-3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,18 +1,28 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+let basket = [ "tomatoes", "rice", "beans", "fritos" ];
 
 function addItem( item ){
   basket.push( item );
-  for (var i = 0; i < basket.length; i++){
-    let item = basket[i];
-    if (item === basket[i]) {
-      contents = true
-    }
+  return true;
+}
+addItem("bread");
+console.log("Was something added to the basket?;", addItem());
+console.log(`Current contents of basket; ${basket}`);
+
+function listItems() {
+  for (let i=0; i<basket.length; i++){
+    console.log(basket[i]);
+    //I cannot figure out how to console out each element of the array to a new line
   }
-return contents;
 }
 
-additem(bread);
-console.log(`Basket contents; ${basket}`);
+function empty(){
+  basket = [];
+  return "basket is empty";
+}
+console.log("What's in the basket?", empty());
+console.log(basket);
+
+//There's no way it was this simple? After spending literal hours on the first two?
