10051af570c978d89885578d076e33004fe9ecb2
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-10-17 11:23:14 -0500

    attempted to complete this challenge

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,18 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem( item ){
+  basket.push( item );
+  for (var i = 0; i < basket.length; i++){
+    let item = basket[i];
+    if (item === basket[i]) {
+      contents = true
+    }
+  }
+return contents;
+}
+
+additem(bread);
+console.log(`Basket contents; ${basket}`);
